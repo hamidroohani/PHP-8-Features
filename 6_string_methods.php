892cc0a785ee49e5407d8fa2ec9116cafcf65f7d
hamid <hamid.dev@yahoo.com>
2021-01-02 08:19:45 +0330

    Create 6_string_methods.php

---
+++ 6_string_methods.php
@@ -0,0 +1,13 @@
+<?php
+
+$str = "Hello World";
+
+if (str_contains($str,'hello') ){ // return true / false
+    echo "exists";
+}
+
+
+
+
+str_starts_with($str,'hello');  // return true
+str_ends_with($str,'ld');  // return true
